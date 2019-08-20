55516676ca8ed56ad0be407e3dfa6fa2c15aaae5
Shreyas Shetty <shreyass2709@gmail.com>
2019-08-20 08:15:13 +0530

    Added User Reducer

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useReducer, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -6,7 +6,8 @@ import IngredientList from './IngredientList';
 import { postIngredient, deleteIngredient } from '../services/ingredient';
 import ErrorModal from '../UI/ErrorModal';
 
-const Ingredients = React.memo(props => {
+// Component Using UseState
+export const Ingredients = React.memo(props => {
   const [ingredients, setIngredients] = useState([]);
   const [isLoading, setLoader] = useState(false);
   const [error, setError] = useState();
@@ -55,7 +56,7 @@ const Ingredients = React.memo(props => {
 
   return (
     <div className="App">
-      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal> }
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
       <IngredientForm addIngredients={addIngredients} isLoading={isLoading} />
 
       <section>
@@ -65,6 +66,127 @@ const Ingredients = React.memo(props => {
       </section>
     </div>
   );
-})
+});
 
-export default Ingredients;
+// Same Component Implementation using useReducer 
+// useReducer Hook usage  
+
+const actionTypes = {
+  SET: 'SET',
+  ADD: 'ADD',
+  DELETE: 'DELETE',
+  REQ: 'REQ',
+  RES: 'RES',
+  ERR: 'ERR',
+  CLR: 'CLR'
+};
+
+const ingredientReducer = (currentState, action) => {
+  switch (action.type) {
+    case actionTypes.SET:
+      return action.ingredients;
+    case actionTypes.ADD:
+      return [...currentState, action.ingredient];
+    case actionTypes.DELETE:
+      return currentState.filter(ing => action.id !== ing.id);
+    default: return currentState;
+  }
+}
+
+const httpStateReducer = (currentState, action) => {
+  switch (action.type) {
+    case actionTypes.REQ:
+      return {
+        ...currentState,
+        loading: true,
+        error: ''
+      };
+    case actionTypes.RES:
+    case actionTypes.CLR:
+      return {
+        ...currentState,
+        loading: false,
+        error: null
+      };
+    case actionTypes.ERR:
+      return {
+        ...currentState,
+        loading: true,
+        error: action.error
+      };
+    default: return currentState;
+  }
+}
+
+const IngredientsRed = React.memo(props => {
+  const [ingredients, dispatch] = useReducer(ingredientReducer, []);
+  const [httpState, dispatchHttp] = useReducer(httpStateReducer, {
+    loading: false,
+    error: null
+  });
+
+  useEffect(() => {
+    console.log('RENDERING Ingredients Component', ingredients);
+  })
+
+  async function addIngredients(ingredient) {
+    try {
+      dispatchHttp({ type: actionTypes.REQ });
+      const response = await postIngredient(ingredient);
+      dispatchHttp({ type: actionTypes.RES });
+      dispatch({
+        type: actionTypes.ADD,
+        ingredient: {
+          id: response.name,
+          ...ingredient
+        }
+      });
+    } catch (error) {
+      dispatchHttp({ type: actionTypes.ERR, error: 'Something went wrong' });
+      console.log('function addIngredients(idingredient)', error);
+    }
+  }
+
+  async function removeIngredients(id) {
+    try {
+      dispatchHttp({ type: actionTypes.REQ });
+      await deleteIngredient(id);
+
+      dispatchHttp({ type: actionTypes.RES });
+      dispatch({
+        type: actionTypes.DELETE,
+        id
+      })
+    } catch (error) {
+      dispatchHttp({ type: actionTypes.ERR, error: 'Something went wrong' });
+      console.log('function removeIngredients(id)', error);
+    }
+  }
+
+  function clearError() {
+    dispatchHttp({ type: actionTypes.CLR });
+  }
+
+  // useCallback is used to avoid re-declacration of a given function  on each render cycle
+  const setFilteredIngs = useCallback(function (ingredients) {
+    dispatch({
+      type: actionTypes.SET,
+      ingredients
+    })
+  }, []);
+
+  return (
+    <div className="App">
+      {httpState.error && <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>}
+      <IngredientForm addIngredients={addIngredients} isLoading={httpState.loading} />
+
+      <section>
+        <Search setFilteredIngs={setFilteredIngs} />
+        {/* Need to add list here! */}
+        <IngredientList ingredients={ingredients} onRemoveItem={removeIngredients} />
+      </section>
+    </div>
+  );
+});
+
+export default IngredientsRed;
